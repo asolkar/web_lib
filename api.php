71988692041afefcca40ed80669fbea6c6e66f41
Mahesh Asolkar <mahesh@mahesha.com>
2011-04-17 17:31:17 -0700

    Added gitignore, fixed paths

--- api.php
+++ api.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once('lib/simplepie/SimplePie.compiled.php');
-require_once('lib/functions.php');
+require_once('simplepie/SimplePie.compiled.php');
+require_once('functions.php');
 
 echo fetch ($_GET['s'], $_GET['u']);
 
